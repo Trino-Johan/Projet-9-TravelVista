c6eafcef7db6934fca66540481b8efa529045ec9
rishnan <johan.trino.1998@gmail.com>
2026-02-06 02:04:30 +0100

    Remove old UIKit files

--- TravelVista/TravelVista/Views/List/ListView.swift
+++ TravelVista/TravelVista/Views/List/ListView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ListView: View {
-    let regions: [Region] = Service().load("Source.json")
+    @StateObject private var viewModel = ListViewModel()
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(regions, id: \.name) { region in
+                ForEach(viewModel.regions, id: \.name) { region in
                     Section(header: Text(region.name)) {
                         ForEach(region.countries, id: \.name) { country in
                             NavigationLink(destination: DetailView(country: country)) {
